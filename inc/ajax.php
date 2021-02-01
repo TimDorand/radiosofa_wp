00140dc9a5af5eb90dccc6cbee7f7eb90f2c17b1
tim.dorand <Quiberon0407GIT@>
2021-02-01 10:16:53 +0100

    Mise à jour du theme

--- inc/ajax.php
+++ inc/ajax.php
@@ -1,6 +1,4 @@
 <?php
-add_action('wp_ajax_load_post', 'rs_load_post');
-add_action('wp_ajax_load_post_with_name', 'rs_load_post_with_name');
 
 function rs_load_post()
 {
@@ -14,8 +12,14 @@ function rs_load_post()
 function rs_load_post_with_name()
 {
     $post_name = $_POST['post_name'];
-    $post = get_page_by_path( $post_name, OBJECT, 'post' );
-    wp_send_json_success($post);
+    try{
+        $post = get_page_by_path( $post_name, OBJECT, 'post' );
+        wp_send_json_success($post);
+    }catch(Exception $e){
+        wp_send_json_error($e);
+    }
     wp_die();
 }
 
+
+add_action('wp_ajax_nopriv_load_post_with_name', 'rs_load_post_with_name');
