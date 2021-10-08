9d54a80e4ee470965522a3d4dab09b3ce7df608a
tim.dorand <Quiberon0407GIT@>
2021-10-08 21:34:30 +0200

    1. Mettre à jour les bon de souscription

--- header.php
+++ header.php
@@ -184,28 +184,27 @@
                    <li class="tablinks currenttab pageTemplateSelector" pageSelector="page-radio-ondes"><h2><a href="/#">Salon</a></h2></li>
                    <li class="tablinks pageSelector" pageSelector="page-sofas"><h2><a href="/#page-sofas">Sofas</a></h2></li>
                    <li class="tablinks pageTemplateSelector" pageSelector="page-radio-convives"><h2><a href="/#radio-convives">Convives</a></h2></li>
-                   <li class="tablinks pageSelector" pageSelector="page-residence"><h2><a href="/#page-residence">Residence</a></h2></li>
+                   <li class="tablinks pageSelector big-phone-only" pageSelector="page-residence"><h2><a href="/#page-residence">Residence</a></h2></li>
                    <li class="tablinks pageSelector desktop-only" pageSelector="page-emissions"><h2><a href="/#page-emissions">Emissions</a></h2></li>
                    <li class="tablinks pageTemplateSelector desktop-only" pageSelector="page-radio-journal"><h2><a href="/#radio-journal">Journal</a></h2></li>
-                   <li><nav class="mobile-only">
-                           <div id="menuToggle">
-                               <!--
-                               A fake / hidden checkbox is used as click reciever,
-                               so you can use the :checked selector on it.
-                               -->
-                               <input type="checkbox" />
-                               <span></span>
-                               <span></span>
-                               <span></span>
-                               <ul id="menuMobile">
-                                   <li class="tablinks pageSelector" pageSelector="page-emissions"><h2><a href="/#page-emissions">Emissions</a></h2></li>
-                                   <li class="tablinks pageTemplateSelector" pageSelector="page-radio-journal"><h2><a href="/#radio-journal">Journal</a></h2></li>
-                               </ul>
-                           </div>
-                       </nav></li>
                </ul>
             </div>
 
+            <nav class="mobile-only">
+                <div class="hamburger-menu">
+                    <input id="menu__toggle" type="checkbox" />
+                    <label class="menu__btn" for="menu__toggle">
+                        <span></span>
+                    </label>
+
+                    <ul class="menu__box">
+                        <li class="tablinks pageSelector" pageSelector="page-emissions"><h2><a href="/#page-emissions">Emissions</a></h2></li>
+                        <li class="tablinks pageTemplateSelector" pageSelector="page-radio-journal"><h2><a href="/#radio-journal">Journal</a></h2></li>
+
+                    </ul>
+                </div>
+            </nav>
+            
 			<?php
 /*			wp_nav_menu(
 				array(
