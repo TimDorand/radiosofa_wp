b5ac18d9b2e75ef5181a549a2f8656946597ed3a
tim.dorand <Quiberon0407GIT@>
2021-11-09 09:05:57 +0100

    Residences + Convives

--- template-parts/content-residence.php
+++ template-parts/content-residence.php
@@ -1,95 +1,58 @@
 <div class="rs-title" style="display:none;width: 200px;height: 32px;"><div style="height: 32px;" id="back-residence"><p>Tous les residents</p></div></div>
 
-<div id="residence-MATSO" class="residence-details">
-    <div class="rs-block-columns">
-        <div class="rs-block-column">
-            <figure class="rs-block-image size-large"><img loading="lazy" width="1024" height="1024" src="http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg" alt="" class="wp-image-964" srcset="http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg 1024w, http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-300x300.jpeg 300w, http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-150x150.jpeg 150w, http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-768x768.jpeg 768w, http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o.jpeg 1092w" sizes="(max-width: 1024px) 100vw, 1024px"></figure>
-        </div>
-        <div class="rs-block-column">
-            <div class="rs-title rs-title-light"><div><h2>MATSO</h2></div></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Neque quisque ante dignissim at eget mauris.<br><br>
-                Nullam amet nulla eget aliquet. Non senectus dictumst at
-                sit elit cursus. Risus euismod diam diam orci fusce eu
-                vitae facilisi. Donec sit amet, id non pretium euismod tempus dui.
-                Purus tristique auctor aliquet ac, in.<br><br>Egestas pretium, cursus
-                lobortis donec senectus sem massa. Ultrices nulla scelerisque nulla
-                senectus id. Vitae, ante volutpat massa elit dolor. Hendrerit orci,
-                integer augue massa mauris ultricies eget ac.</p>
-        </div>
-    </div>
-    <div class="rs-block-columns">
-        <div class="rs-block-column">
-            <div class="rs-title rs-title-light"><div><h2 style="width:30vw">ARCHIVES</h2></div></div>
-        </div>
-    </div>
-    <div class="rs-block-columns residence-details-archives">
-    <?php
-    $categoryArticles = get_category_by_slug("Matso");
-    $posts = get_posts(["numberposts" => -1, "category" => $categoryArticles->cat_ID]);
-
-    foreach ($posts as $post) {
-        $image = get_the_post_thumbnail($post, ["300", "300"]);
-        echo '<div data-post-id="' . $post->post_name . '" class="convives-post">';
-        echo $image;
-        echo $post->post_content;
-        echo '</div>';
-    }
-    ?>
-    </div>
-    <?php wp_reset_postdata(); ?>
-</div>
-<div class="rs-block-columns replay-images">
-    <div class="rs-block-column">
-        <div class="rs-title rs-title-light">
-            <div><h2>MATSO</h2></div>
-        </div>
-
-        <figure class="rs-block-image size-large is-resized"><img
-                    src="/wp-content/uploads/2021/01/tom-coololo-edited-1024x1024.jpeg" alt=""
-                    class="wp-image-528" width="376" height="376"/>
-        </figure>
-    </div>
-
-    <div class="rs-block-column">
-        <div class="rs-title rs-title-default">
-            <div><h2>NOFRAJE</h2></div>
-        </div>
-
-        <figure class="rs-block-image size-large is-resized"><img
-                    src="/wp-content/uploads/2020/12/marley-marl-in-control-volume-1.jpg" alt=""
-                    class="wp-image-447" width="204" height="204"/>
-        </figure>
-    </div>
-
-    <div class="rs-block-column">
-        <div class="rs-title rs-title-dark">
-            <div><h2>CHARLEEPS</h2></div>
-        </div>
-
-        <figure class="rs-block-image size-large is-resized"><img
-                    src="/wp-content/uploads/2021/01/Baume-1-1024x1024.jpg" alt=""
-                    class="wp-image-526" width="204" height="204"/>
-        </figure>
-    </div>
-</div>
 <?php
-$residents = [
-    "matso" => ["name" => "matso",
-        "desc" => "test",
-        "image" => "/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg\" alt=\"\" class=\"wp-image-964\" srcset=\"http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg"],
-    "mimi" => [
-        "name" => "mimi",
-        "desc" => "test",
-        "image" => "/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg\" alt=\"\" class=\"wp-image-964\" srcset=\"http://localhost:8888/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg"]
-];
+
+class Resident {
+    public $name;
+    public $desc;
+    public $image;
+
+    function __construct($name, $desc, $image) {
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->image = $image;
+    }
+}
+
+$matso = new Resident(
+        "MATSO",
+    "test",
+    "/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg");
+
+$broski = new Resident(
+        "BROSKI",
+    "test",
+    "/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg");
+
+$mimi = new Resident(
+        "MIMI",
+    "test",
+    "/wp-content/uploads/2021/03/89092659_1117093465349544_8177255567056699392_o-1024x1024.jpeg");
+
+
+$residents = [$matso, $broski, $mimi];
+
+echo '<div class="rs-block-columns replay-images">';
+foreach ($residents as $resident) {
+echo '<div class="rs-block-column">
+        <div class="rs-title rs-title-light">
+            <div><h2>' . $resident->name . '</h2></div>
+        </div>
+
+        <figure class="rs-block-image size-large is-resized">
+        <img src="' . $resident->image . '" alt="' . $resident->name . '" width="376" height="376"/>
+        </figure>
+    </div>';
+}
+echo '</div>';
 
 foreach ($residents as $resident) {
     echo '
     <div id="residence-' . $resident->name . '" class="residence-details">
     <div class="rs-block-columns">
         <div class="rs-block-column">
-            <figure class="rs-block-image size-large"><img loading="lazy" width="1024" height="1024" src="' . $resident->image . '"></figure>
+            <figure class="rs-block-image size-large">
+            <img loading="lazy" width="500" height="500" src="' . $resident->image . '" alt="' . $resident->name . '"></figure>
         </div>
         <div class="rs-block-column">
             <div class="rs-title rs-title-light"><div><h2>' . $resident->name . '</h2></div></div>
@@ -113,7 +76,7 @@ foreach ($residents as $resident) {
         echo $post->post_content;
         echo '</div>';
     }
-    echo '</div>';
+    echo '</div></div>';
     /*wp_reset_postdata();*/
 }
 ?>
