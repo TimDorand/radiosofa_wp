e53bcc3ad35a27eb2599697e76ce4ece3324be5a
Tim <tim.dorand@gmail.com>
2023-12-06 16:00:35 +0100

    Get id instead of name

--- js/utils.js
+++ js/utils.js
@@ -204,7 +204,7 @@ function handleResidence() {
 function showResidenceDetails(e) {
     var resident = e.currentTarget.children[1].innerText
     $("#page-radio-residence .replay-images").hide();
-    $("#residence-" + resident.replace('/','');.split(' ').join('_')).show();
+    $("#residence-" + resident.replaceAll('/','').split(' ').join('_')).show();
     $("#back-residence").parent("div").show();
     handleReplayIframe();
     setTimeout(function () {
